c95bf047ff1fe1fd0b8dd1de8b1cdb4dbe31f03c
codedust <codedust@users.noreply.github.com>
2015-11-22 19:51:18 +0100

    Apply PEP8 code style guidelines

--- src/interfaces/SamplerInterface.py
+++ src/interfaces/SamplerInterface.py
@@ -4,23 +4,23 @@ Created on 21.11.2015
 @author: Moritz
 '''
 
+
 class SamplerInterface(object, DataManipulatorInterface):
     '''
-    The SamplerInterface 
+    The SamplerInterface
     '''
-
 
     def __init__(self):
         '''
         Constructor
         '''
-        super(DataManipulatorInterface, self).__init__();
-    
+        super(DataManipulatorInterface, self).__init__()
+
     def setSamplerIteration(self, iteration):
         raise NotImplementedError("Not implemented")
-    
+
     def appendNewSamples(self):
         raise NotImplementedError("Not implemented")
-    
+
     def createSamples(self, newData, *args):
         raise NotImplementedError("Not implemented")
--- tests/testDataManager.py
+++ tests/testDataManager.py
@@ -1,37 +1,37 @@
 import unittest
 import sys
-
-sys.path.append( '../src/data' )
+sys.path.append('../src/data')
 from DataEntry import DataEntry
 from DataManager import DataManager
 
 
 class testDataManager(unittest.TestCase):
-  def test_init(self):
-      dataManager = DataManager('episodes')
-      self.assertIsInstance(dataManager, DataManager)
-      self.assertEqual(dataManager.name, 'episodes')
+    def test_init(self):
+        dataManager = DataManager('episodes')
+        self.assertIsInstance(dataManager, DataManager)
+        self.assertEqual(dataManager.name, 'episodes')
 
-  def test_subDataManager(self):
-      dataManager = DataManager('episodes')
-      subDataManager = DataManager('steps')
-      subSubDataManager = DataManager('subSteps')
+    def test_subDataManager(self):
+        dataManager = DataManager('episodes')
+        subDataManager = DataManager('steps')
+        subSubDataManager = DataManager('subSteps')
 
-      dataManager.subDataManager = subDataManager
-      subDataManager.subDataManager = subSubDataManager
+        dataManager.subDataManager = subDataManager
+        subDataManager.subDataManager = subSubDataManager
 
-      self.assertIs(dataManager.subDataManager, subDataManager)
-      self.assertIs(dataManager.subDataManager.subDataManager, subSubDataManager)
+        self.assertIs(dataManager.subDataManager, subDataManager)
+        self.assertIs(dataManager.subDataManager.subDataManager,
+                      subSubDataManager)
 
-  def test_addDataEntry(self):
-      dataManager = DataManager('episodes')
-      dataManager.addDataEntry('parameters', 5, -1, 1)
+    def test_addDataEntry(self):
+        dataManager = DataManager('episodes')
+        dataManager.addDataEntry('parameters', 5, -1, 1)
 
-      self.assertIsInstance(dataManager.dataEntries, dict)
-      self.assertIsInstance(dataManager.dataEntries['parameters'], DataEntry)
-      self.assertEqual(dataManager.dataEntries['parameters'].size, 5)
-      self.assertEqual(dataManager.dataEntries['parameters'].minValue, -1)
-      self.assertEqual(dataManager.dataEntries['parameters'].maxValue, 1)
+        self.assertIsInstance(dataManager.dataEntries, dict)
+        self.assertIsInstance(dataManager.dataEntries['parameters'], DataEntry)
+        self.assertEqual(dataManager.dataEntries['parameters'].size, 5)
+        self.assertEqual(dataManager.dataEntries['parameters'].minValue, -1)
+        self.assertEqual(dataManager.dataEntries['parameters'].maxValue, 1)
 
 if __name__ == '__main__':
     unittest.main()
