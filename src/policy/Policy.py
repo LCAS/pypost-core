562524693572150d4abbcf2a3ba8b14fd96f8c4e
Moritz Willig <moritz.willig@gmail.com>
2015-12-11 15:13:54 +0100

    expanded policy class

--- src/policy/Policy.py
+++ src/policy/Policy.py
@@ -31,7 +31,25 @@ class Policy(object):
 
         if policyName is None:
             policyName = 'sample' + poolName[0].upper() + poolName[1:]
-        self._poolName = policyName
+        self._policyName = policyName
         '''
         Unique policy name
         '''
+
+    def getPolicyFunction(self):
+        return self._policyFunction
+
+    def getPolicyName(self):
+        return self._policyName
+
+    def getPoolName(self):
+        return self._policyName
+
+    def __call__(self, *args, **kw):
+        '''
+        Execute the policy function and get the computed result.
+        Always use this method to execute the policy function and do not
+        call the stored policy function itself, as the internal structure may
+        perform additional actions!
+        '''
+        return self.getPolicyFunction()(*args, **kw)
