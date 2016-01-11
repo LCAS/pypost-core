5934d391c666eca91b5837b5124f7279d1146261
Sebastian Kreutzer <SebastianKreutzer@gmx.net>
2016-01-11 14:02:09 +0100

    Modify code coverage tool to exclude certain files

--- tools/computeCodeConverage.py
+++ tools/computeCodeConverage.py
@@ -2,7 +2,7 @@ import coverage
 import unittest
 
 if __name__ == '__main__':
-    c = coverage.Coverage(branch=True, source=['../src/'])
+    c = coverage.Coverage(branch=True, source=['../src/'], omit=['*Interface*', '*__init__.py'])
     
     c.start()
     
