3d358a80ce8f7234dd122fc0048cda2c48978d04
codedust <codedust@users.noreply.github.com>
2016-01-17 22:54:50 +0100

    Stop generating coverage html report

--- tools/computeCodeConverage.py
+++ tools/computeCodeConverage.py
@@ -3,12 +3,12 @@ import unittest
 
 if __name__ == '__main__':
     c = coverage.Coverage(branch=True, source=['../src/'], omit=['*Interface*', '*__init__.py'])
-    
+
     c.start()
-    
+
     suite = unittest.TestLoader().discover('../tests', 'test*')
     unittest.TextTestRunner().run(suite)
-   
+
     c.stop()
     c.report()
-    c.html_report()
+    # c.html_report()
