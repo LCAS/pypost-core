cb9de4ab505e95b6bf5606a5adb36b6cff5ff25d
Moritz Willig <moritz.willig@gmail.com>
2016-01-24 23:46:56 +0100

    added distribution tests to coverage test

--- tools/computeCodeConverage.py
+++ tools/computeCodeConverage.py
@@ -1,12 +1,26 @@
+'''
+This tool requires coverage.py to be installed. See
+https://coverage.readthedocs.org/en/coverage-4.0.3/install.html
+for more information.
+'''
+
 import coverage
 import unittest
 
 if __name__ == '__main__':
-    c = coverage.Coverage(branch=True, source=['../src/'], omit=['*Interface*', '*__init__.py'])
+    c = coverage.Coverage(
+        branch=True,
+        source=['../src/'],
+        omit=[
+            '*Interface*',
+            '*__init__.py'])
 
     c.start()
 
     suite = unittest.TestLoader().discover('../tests', 'test*')
+    unittest.TextTestRunner().run(suite)
+
+    suite = unittest.TestLoader().discover('../tests/distribution', 'test*')
     unittest.TextTestRunner().run(suite)
 
     c.stop()
