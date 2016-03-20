05746917d622454932a8de5840bdece4f61ee775
codedust <codedust@users.noreply.github.com>
2016-03-20 20:55:43 +0100

    Increase coverage

---
+++ tests/examples/testPowerRosenbrock.py
@@ -0,0 +1,7 @@
+import unittest
+from examples.stochasticSearch.rosenbrock.Power_Rosenbrock import PowerRosenbrock
+
+class testPowerRosenbrock(unittest.TestCase):
+
+    def testPowerRosenbrock(self):
+        pass
