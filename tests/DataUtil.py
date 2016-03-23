351390bfbd9f354409dd7a0ec5b392fce1c4ddbe
Fabio Boschert <fd.boschert@gmail.com>
2016-03-23 18:38:36 +0100

    New help function for testing Evaluators

--- tests/DataUtil.py
+++ tests/DataUtil.py
@@ -23,3 +23,21 @@ def createTestManager():
     subSubDataManager.addDataEntry('subStates', 1)
     subSubDataManager.addDataEntry('subActions', 2)
     return dataManager
+
+
+def createTestManager2():
+    dataManager = DataManager('episodes2')
+    subDataManager = DataManager('steps2')
+    subSubDataManager = DataManager('subSteps2')
+
+    dataManager.subDataManager = subDataManager
+    subDataManager.subDataManager = subSubDataManager
+
+    dataManager.addDataEntry('parameters', 5, -100, 100)
+    dataManager.addDataEntry('context', 4, -100, 100)
+    dataManager.addDataEntry('goals', 8, -100, 100)
+    subDataManager.addDataEntry('states', 1, -100, 100)
+    subDataManager.addDataEntry('actions', 2)
+    subSubDataManager.addDataEntry('subStates', 1)
+    subSubDataManager.addDataEntry('subActions', 2)
+    return dataManager
